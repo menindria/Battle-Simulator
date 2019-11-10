32d967a4961452f51af3454fb0e1888b7e21124a
Jovan Jeremic <jovanjeremic@gmail.com>
2019-11-10 10:33:38 +0100

    Fixed bug

--- BattleSimulator.Domain/Army.cs
+++ BattleSimulator.Domain/Army.cs
@@ -5,11 +5,13 @@ namespace BattleSimulator.Domain
 {
     public class Army : EntityBase
     {
+        public Battle Battle { get; private set; }
+        public int BattleId { get; private set; }
         public int NumberOfAttacks { get; private set; }
         public string Name { get; private set; }
         public int NumberOfUnits { get; private set; } //TODO:JJ Can be used type with smaller size
         public StrategyAndAttackOptions StrategyAndAttackOption { get; private set; }
-        public bool IsDead => NumberOfAttacks >= NumberOfUnits;
+        public bool IsDead => NumberOfAttacks*2 >= NumberOfUnits;
         public List<Log> Logs { get; set; } = new List<Log>();
         protected Army() { }
 
